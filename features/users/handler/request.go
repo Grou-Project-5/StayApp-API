5e70ec00dd1bbe66f2098844999eb0ee39072aa7
Rizal Fahmi <rizalfahmi3002@gmail.com>
2023-03-14 10:36:48 +0700

    add register request struct

--- features/users/handler/request.go
+++ features/users/handler/request.go
@@ -1,1 +1,10 @@
 package handler
+
+type RegisterRequest struct {
+	Name     string `json:"name" form:"name"`
+	Email    string `json:"email" form:"email"`
+	Password string `json:"password" form:"password"`
+	Address  string `json:"address" form:"address"`
+	Gender   string `json:"gender" form:"gender"`
+	Phone    string `json:"phone" form:"phone"`
+}
