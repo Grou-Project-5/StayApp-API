36a5d226df0f9036ad8e668fbe36ffd4db265422
Rizal Fahmi <rizalfahmi3002@gmail.com>
2023-03-15 02:04:20 +0700

    create feature add new feedback

--- features/feedback/data/query.go
+++ features/feedback/data/query.go
@@ -1,1 +1,27 @@
 package data
+
+import (
+	"StayApp-API/features/feedback"
+
+	"gorm.io/gorm"
+)
+
+type feedbackQuery struct {
+	db *gorm.DB
+}
+
+func New(db *gorm.DB) feedback.FeedbackData {
+	return &feedbackQuery{
+		db: db,
+	}
+}
+
+// Add implements feedback.FeedbackData
+func (fq *feedbackQuery) Add(newFeedback feedback.Core) error {
+	data := CoreToFeedback(newFeedback)
+	tx := fq.db.Create(&data)
+	if tx.Error != nil {
+		return tx.Error
+	}
+	return nil
+}
