ac569f00077bb748ae3914b9ecf3e2c33b546ee6
Rizal Fahmi <rizalfahmi3002@gmail.com>
2023-03-14 10:17:09 +0700

    create model room

--- features/rooms/data/model.go
+++ features/rooms/data/model.go
@@ -1,1 +1,22 @@
 package data
+
+import (
+	"gorm.io/gorm"
+)
+
+type Room struct {
+	gorm.Model
+	UserID      int
+	Name        string
+	Price       int
+	Description string
+	Location    string
+	UserName    string
+	Pictures    []Pictures
+}
+
+type Pictures struct {
+	gorm.Model
+	URL    string
+	RoomID uint
+}
