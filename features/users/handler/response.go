0cbd33417f04be7647ed08a7c8c02da1896b7558
Rizal Fahmi <rizalfahmi3002@gmail.com>
2023-03-14 10:56:10 +0700

    add response user

--- features/users/handler/response.go
+++ features/users/handler/response.go
@@ -5,3 +5,13 @@ type MiniResponse struct {
 	Name     string `json:"name" form:"name"`
 	Pictures string `json:"pictures" form:"pictures"`
 }
+
+type UserResponse struct {
+	Id       uint   `json:"id" form:"id"`
+	Name     string `json:"name" form:"name"`
+	Email    string `json:"email" form:"email"`
+	Address  string `json:"address" form:"address"`
+	Phone    string `json:"phone" form:"phone"`
+	Gender   string `json:"gender" form:"gender"`
+	Pictures string `json:"pictures" form:"pictures"`
+}
