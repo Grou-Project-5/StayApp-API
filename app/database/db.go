f1b9f8fd94ab9cab8a99c1580c983fcb8d492d8b
Rizal Fahmi <rizalfahmi3002@gmail.com>
2023-03-14 09:46:18 +0700

    create config

--- app/database/db.go
+++ app/database/db.go
@@ -1,1 +1,28 @@
-package db
+package database
+
+import (
+	"fmt"
+	"StayApp-API/app/config"
+	"log"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func InitDBMySql(cfg config.AppConfig) *gorm.DB {
+	connectionString := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8&parseTime=True&loc=Local",
+		cfg.DB_USER,
+		cfg.DB_PASS,
+		cfg.DB_HOST,
+		cfg.DB_PORT,
+		cfg.DB_NAME,
+	)
+
+	db, err := gorm.Open(mysql.Open(connectionString), &gorm.Config{})
+
+	if err != nil {
+		log.Println("error connect to database:", err.Error())
+		return nil
+	}
+	return db
+}
--- main.go
+++ main.go
@@ -1,1 +1,20 @@
 package main
+
+import (
+	"StayApp-API/app/config"
+	"StayApp-API/app/database"
+	"StayApp-API/app/router"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func main() {
+	cfg := config.InitConfig()
+	db := database.InitDBMySql(*cfg)
+
+	e := echo.New()
+	e.Use(middleware.CORS())
+	router.InitRouter(db, e)
+	e.Logger.Fatal(e.Start(":80"))
+}
