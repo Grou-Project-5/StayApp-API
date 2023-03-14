6f2fbd0ad5265039ad66eeb68a67dc8aa39cb24e
Rizal Fahmi <rizalfahmi3002@gmail.com>
2023-03-14 10:15:36 +0700

    create model user

--- features/users/data/model.go
+++ features/users/data/model.go
@@ -1,1 +1,21 @@
 package data
+
+import (
+	modelRoom "StayApp-API/features/rooms/data"
+	modelFeedback "StayApp-API/features/feedback/data"
+
+	"gorm.io/gorm"
+)
+
+type User struct {
+	gorm.Model
+	Name     string
+	Email    string `gorm:"unique"`
+	Password string
+	Address  string
+	Phone    string
+	Gender   string `gorm:"type:enum('Male', 'Female')"`
+	Pictures string
+	Rooms    []modelRoom.Room
+	Feedback []modelFeedback.Feedback
+}
