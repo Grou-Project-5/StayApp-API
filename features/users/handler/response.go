6109ba4b0e9fdaf4f590956d27744520e0df0dd8
Rizal Fahmi <rizalfahmi3002@gmail.com>
2023-03-14 10:50:03 +0700

    add struct mini response for user

--- features/users/handler/response.go
+++ features/users/handler/response.go
@@ -1,1 +1,7 @@
 package handler
+
+type MiniResponse struct {
+	Id       uint   `json:"id" form:"id"`
+	Name     string `json:"name" form:"name"`
+	Pictures string `json:"pictures" form:"pictures"`
+}
