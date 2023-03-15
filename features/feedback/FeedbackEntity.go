79ac35031d8c52fd3612ecb17b06a2e7e8074b14
Rizal Fahmi <rizalfahmi3002@gmail.com>
2023-03-15 16:01:39 +0700

    fix core and add entity list

--- features/feedback/FeedbackEntity.go
+++ features/feedback/FeedbackEntity.go
@@ -1,18 +1,21 @@
 package feedback
 
 type Core struct {
-	Id       uint
-	RoomID   uint
-	UserID   uint
-	UserName string
-	Rating   int    `validate:"required"`
-	Comment  string `validate:"required"`
+	ID        uint
+	RoomID    uint
+	UserID    uint
+	UserName  string
+	Rating    int    `validate:"required"`
+	Comment   string `validate:"required"`
+	CreatedAt string
 }
 
 type FeedbackService interface {
 	Add(newFeedback Core) error
+	List(roomiD int) ([]Core, error)
 }
 
 type FeedbackData interface {
 	Add(newFeedback Core) error
+	List(roomID int) ([]Core, error)
 }
