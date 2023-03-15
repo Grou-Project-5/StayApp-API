6fbb5570befe83f7d4a8e73ba6544cd2b1a476cd
Rizal Fahmi <rizalfahmi3002@gmail.com>
2023-03-15 16:02:29 +0700

    add feedback response

--- features/feedback/handler/response.go
+++ features/feedback/handler/response.go
@@ -1,1 +1,11 @@
 package handler
+
+type FeedbackResponse struct {
+	ID        uint   `json:"id"`
+	UserName  string `json:"user_name"`
+	Rating    int    `json:"rating"`
+	Comment   string `json:"comment"`
+	CreatedAt string `json:"created_at"`
+}
+
+type ListFeedbackResponse []FeedbackResponse
