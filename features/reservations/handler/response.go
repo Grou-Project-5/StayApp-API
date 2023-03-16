d35933089e96b6f762f4b2e3ef57dc3d760cd7f4
Rizal Fahmi <rizalfahmi3002@gmail.com>
2023-03-17 01:12:27 +0700

    add payment status response

--- features/reservations/handler/response.go
+++ features/reservations/handler/response.go
@@ -8,3 +8,8 @@ type PaymentResponse struct {
 	OrderID     string `json:"order_id"`
 	RedirectURL string `json:"redirect_url"`
 }
+
+type PayStatusResponse struct {
+	OrderID       string `json:"order_id"`
+	StatusPayment string `json:"payment_status"`
+}
