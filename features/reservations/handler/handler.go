4443e93947d77777746da0934bc1113893cffecb
Rizal Fahmi <rizalfahmi3002@gmail.com>
2023-03-16 15:17:05 +0700

    fix feature add reservation

--- features/reservations/handler/handler.go
+++ features/reservations/handler/handler.go
@@ -5,6 +5,8 @@ import (
 	"StayApp-API/middlewares"
 	"StayApp-API/utils/helper"
 	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/jinzhu/copier"
 	"github.com/labstack/echo/v4"
@@ -25,14 +27,18 @@ func (rm *ReservationHandler) Check(c echo.Context) error {
 	if err := c.Bind(&checkInput); err != nil {
 		return c.JSON(helper.ErrorResponse(err))
 	}
-	tmp, err := rm.srv.Check(int(checkInput.RoomID), checkInput.StartDate, checkInput.EndDate)
+
+	checkAvailability := reservations.Core{}
+	copier.Copy(&checkAvailability, &checkInput)
+
+	tmp, err := rm.srv.Check(checkAvailability)
 	if err != nil {
 		return c.JSON(helper.ErrorResponse(err))
 	}
 	res := Availability{}
 	var message string
 	res.Availability = tmp
-	if tmp == false {
+	if !tmp {
 		message = "room not available"
 	} else {
 		message = "room available"
@@ -45,12 +51,24 @@ func (rm *ReservationHandler) Add(c echo.Context) error {
 	if err := c.Bind(&newReservationInput); err != nil {
 		return c.JSON(helper.ErrorResponse(err))
 	}
+
+	starDate, _ := time.Parse("2006-01-02", newReservationInput.StartDate)
+	endDate, _ := time.Parse("2006-01-02", newReservationInput.EndDate)
+	newReservationInput.Days = int64((endDate.Sub(starDate).Hours() / 24) + 1)
+	
+	now := time.Now()
+	unixTimeNano := now.UnixNano()
+	newReservationInput.OrderID = "HAPP-" + strconv.FormatInt(unixTimeNano, 10)
 	newReservationInput.UserID = uint(middlewares.ExtractToken(c))
+
 	newReservation := reservations.Core{}
 	copier.Copy(&newReservation, &newReservationInput)
-	err := rm.srv.Add(newReservation)
+	orderID, data, err := rm.srv.Add(newReservation)
 	if err != nil {
 		return c.JSON(helper.ErrorResponse(err))
 	}
-	return c.JSON(helper.SuccessResponse(http.StatusOK, "reservation success"))
+	res := PaymentResponse{}
+	res.OrderID = orderID
+	res.RedirectURL = data.RedirectURL
+	return c.JSON(helper.SuccessResponse(http.StatusOK, "reservation success, waiting for payment", res))
 }
