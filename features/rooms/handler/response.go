43c676091ae89dfe0b880661978ec850de207b51
Yuda <adiyudapranata@gmail.com>
2023-03-16 08:05:32 +0700

    Fix: Room feature

--- features/rooms/handler/response.go
+++ features/rooms/handler/response.go
@@ -1,33 +1,71 @@
 package handler
 
-import "StayApp-API/features/rooms"
+import (
+	"StayApp-API/features/rooms"
+
+	"gorm.io/gorm"
+)
+
+type Pictures struct {
+	gorm.Model
+	URL    string
+	RoomID uint
+}
 
 type RoomResponse struct {
+	ID                uint   `json:"id"`
+	Name              string `json:"name" form:"name"`
+	Price             int    `json:"price" form:"price"`
+	Description       string `json:"description" form:"description"`
+	Location          string `json:"location" form:"location"`
+	MaxVisitors       int    `json:"max_visitors" form:"max_visitors"`
+	SpecialAccess     string `json:"special_access" form:"special_access"`
+	Bedroom           int    `json:"bedroom" form:"bedroom"`
+	RoomTotal         int    `json:"room_total" form:"room_total"`
+	Kitchen           string `json:"kitchen" form:"kitchen"`
+	Wifi              string `json:"wifi" form:"wifi"`
+	Garage            string `json:"garage" form:"garage"`
+	ExcellentFeatures string `json:"excellent_features" form:"excellent_features"`
+	CleaningFee       int    `json:"cleaning_fee" form:"cleaning_fee"`
+	ServiceFee        int    `json:"service_fee" form:"service_fee"`
+	BringAnimal       string `json:"bring_animal" form:"bring_animal"`
+	CheckIn           string `json:"check_in" form:"check_in"`
+	CheckOut          string `json:"check_out" form:"check_out"`
+	TakePhoto         string `json:"take_photo" form:"take_photo"`
+	Other             string `json:"other" form:"other"`
+	UserID            uint   `json:"user_id" form:"user_id"`
+	UserName          string `json:"user_name" form:"user_name"`
+	Pictures          []Pictures
+}
+type AllRoomResponse struct {
 	ID          uint   `json:"id"`
 	Name        string `json:"name" form:"name"`
 	Price       int    `json:"price" form:"price"`
 	Description string `json:"description" form:"description"`
 	Location    string `json:"location" form:"location"`
-	UserID      uint    `json:"user_id" form:"user_id"`
 	UserName    string `json:"user_name" form:"user_name"`
+	Pictures    []Pictures
 }
 
-func CoreToGetAllRoomResp(data []rooms.Core) []RoomResponse {
-	res := []RoomResponse{}
+func CoreToGetAllRoomResp(data []rooms.Core) []AllRoomResponse {
+	res := []AllRoomResponse{}
 	for _, val := range data {
-		res = append(res, CoreToGetRoomResp(val))
+		res = append(res, CoreToGetAllRoomRespB(val))
 	}
 	return res
 }
 
-func CoreToGetRoomResp(data rooms.Core) RoomResponse {
-	return RoomResponse{
-		ID:          uint(data.ID),
+
+func CoreToGetAllRoomRespB(data rooms.Core) AllRoomResponse {
+	return AllRoomResponse{
+		ID:          data.ID,
 		Name:        data.Name,
 		Price:       data.Price,
 		Description: data.Description,
 		Location:    data.Location,
-		UserID:      data.UserID,
 		UserName:    data.UserName,
+		Pictures:    []Pictures{},
 	}
 }
+
+
