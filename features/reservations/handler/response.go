7d455d00b9ea98ae345cd13d795d5e88c1481926
Rizal Fahmi <rizalfahmi3002@gmail.com>
2023-03-16 15:16:38 +0700

    add respons for payment response

--- features/reservations/handler/response.go
+++ features/reservations/handler/response.go
@@ -3,3 +3,8 @@ package handler
 type Availability struct {
 	Availability bool `json:"availability"`
 }
+
+type PaymentResponse struct {
+	OrderID     string `json:"order_id"`
+	RedirectURL string `json:"redirect_url"`
+}
