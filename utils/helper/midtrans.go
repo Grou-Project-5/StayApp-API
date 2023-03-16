496669f896982fca569f9bf78a33405d302fc78f
Rizal Fahmi <rizalfahmi3002@gmail.com>
2023-03-16 15:11:36 +0700

    create function midtrans payment gateway

---
+++ utils/helper/midtrans.go
@@ -0,0 +1,30 @@
+package helper
+
+import (
+	"StayApp-API/app/config"
+
+	"github.com/midtrans/midtrans-go"
+	"github.com/midtrans/midtrans-go/coreapi"
+	"github.com/midtrans/midtrans-go/snap"
+)
+
+func MidtransPay(OrderID string, GrossAmount int64) *snap.Response {
+	// Set up Midtrans configuration
+	midtrans.ServerKey = config.MidtransServerKey
+	midtrans.ClientKey = config.MidtransClientKey
+	midtrans.Environment = midtrans.Sandbox
+	c := coreapi.Client{}
+	c.New(config.MidtransServerKey, midtrans.Sandbox)
+		// Create Snap request
+		req := &snap.Request{
+			TransactionDetails: midtrans.TransactionDetails{
+				OrderID:  OrderID,
+				GrossAmt: GrossAmount,
+			},
+			CreditCard: &snap.CreditCardDetails{
+				Secure: true,
+			},
+		}
+		snapResp, _ := snap.CreateTransaction(req)
+		return snapResp
+}
