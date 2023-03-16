4f57806c2a86502d473502b571d75bb94581103d
Rizal Fahmi <rizalfahmi3002@gmail.com>
2023-03-16 17:36:04 +0700

    fix change data type

--- features/feedback/handler/request.go
+++ features/feedback/handler/request.go
@@ -1,9 +1,9 @@
 package handler
 
 type NewFeedbackRequest struct {
-	RoomID    uint	`json:"room_id"`
+	RoomID    uint `json:"room_id"`
 	UserID    uint
 	CreatedAt string
-	Rating    int    `json:"rating"`
-	Comment   string `json:"comment"`
+	Rating    float64 `json:"rating"`
+	Comment   string  `json:"comment"`
 }
--- features/feedback/handler/response.go
+++ features/feedback/handler/response.go
@@ -1,11 +1,11 @@
 package handler
 
 type FeedbackResponse struct {
-	ID        uint   `json:"id"`
-	UserName  string `json:"user_name"`
-	Rating    int    `json:"rating"`
-	Comment   string `json:"comment"`
-	CreatedAt string `json:"created_at"`
+	ID        uint    `json:"id"`
+	UserName  string  `json:"user_name"`
+	Rating    float64 `json:"rating"`
+	Comment   string  `json:"comment"`
+	CreatedAt string  `json:"created_at"`
 }
 
 type ListFeedbackResponse []FeedbackResponse
