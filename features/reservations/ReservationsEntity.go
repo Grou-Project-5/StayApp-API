f0e452ce11591761ce8afde94645cc69c275d323
Rizal Fahmi <rizalfahmi3002@gmail.com>
2023-03-17 00:59:35 +0700

    add field core and entity CheckOwner & PayStatus

--- features/reservations/ReservationsEntity.go
+++ features/reservations/ReservationsEntity.go
@@ -13,16 +13,20 @@ type Core struct {
 	Days          int64
 	GrossAmount   int64
 	OrderID       string
-	StatusPaymen  string
+	StatusPayment string
+	RedirectUrl   string
 }
 
 type ReservationService interface {
 	Check(checkAvailability Core) (bool, error)
 	Add(newReservation Core) (string, *snap.Response, error)
+	PayStatus(updatePayStatus Core) (Core, error)
 }
 
 type ReservationData interface {
 	Check(checkAvailability Core) (bool, error)
 	GrossAmt(roomID int, days int64) int64
+	CheckOwner(roomID, userID int) bool
 	Add(newReservation Core) error
+	PayStatus(updatePayStatus Core) error
 }
