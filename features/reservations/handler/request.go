bec268762c17a51fbc6186467acee457665c4bee
Rizal Fahmi <rizalfahmi3002@gmail.com>
2023-03-16 10:51:32 +0700

    fix reservasion request

--- features/reservations/handler/request.go
+++ features/reservations/handler/request.go
@@ -1,7 +1,8 @@
 package handler
 
-type CheckRequest struct {
-	RoomID   uint   `json:"name"`
-	StartDate string `json:"start_date"`
-	EndDate  string `json:"end_date"`
+type ReservationRequest struct {
+	RoomID        uint `json:"room_id"`
+	UserID        uint
+	StartDate     string `json:"start_date"`
+	EndDate       string `json:"end_date"`
 }
